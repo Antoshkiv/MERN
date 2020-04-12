e16b0af6fb7b757f23ad09345d3bcc24e39b7c69
Dima <dima@MacBook-Pro-Dima.local>
2020-04-12 16:24:21 +0300

    add edit place ui

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Input from '../../shared/components/FormElements/Input';
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from '../../shared/util/validators';
+import Button from '../../shared/components/FormElements/Button';
+
+import './placeForm.css';
+import { useForm } from '../../shared/hooks/form-hook';
 
 const DUMMY_PLACES = [
   {
@@ -35,11 +44,57 @@ const UpdatePlace = () => {
 
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true,
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true,
+      },
+    },
+    true
+  );
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log('state', formState.inputs);
+  };
+
   if (!identifiedPlace) {
-    return <div>Could not find place!</div>;
+    return <div className="center">Could not find place!</div>;
   }
 
-  return <div>UpdatePlace work</div>;
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialIsValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialIsValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
 };
 
 export default UpdatePlace;
